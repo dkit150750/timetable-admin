4ff010ce84ac80caa02f79e19dd0ae2ce6648c98
dkit150750 <dkit150750@gmail.com>
2021-11-06 18:16:00 +0300

    first commit

---
+++ src/router/routes.ts
@@ -0,0 +1,24 @@
+import { Pinia } from 'pinia';
+import { RouteRecordRaw } from 'vue-router';
+
+export default function (store: Pinia) {
+  if (process.env.CLIENT && process.env.DEV) {
+    console.log(store);
+  }
+  const routes: RouteRecordRaw[] = [
+    {
+      path: '/',
+      component: () => import('layouts/MainLayout.vue'),
+      children: [{ path: '', component: () => import('pages/Index.vue') }],
+    },
+
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+      path: '/:catchAll(.*)*',
+      component: () => import('pages/Error404.vue'),
+    },
+  ];
+
+  return routes;
+}
