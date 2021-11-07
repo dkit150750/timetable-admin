2d63d658eced131692aba6b46fdef6cf1d14b987
dkit150750 <dkit150750@gmail.com>
2021-11-07 12:30:17 +0300

    pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -14,6 +14,62 @@ export default function (store: Pinia) {
 			],
 		},
 
+		{
+			path: '/groups',
+			component: () => import('layouts/MainLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Groups.vue') },
+			],
+		},
+
+		{
+			path: '/changes',
+			component: () => import('layouts/MainLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Changes.vue') },
+			],
+		},
+
+		{
+			path: '/teachers',
+			component: () => import('layouts/MainLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Teachers.vue') },
+			],
+		},
+
+		{
+			path: '/disciplines',
+			component: () => import('layouts/MainLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Disciplines.vue') },
+			],
+		},
+
+		{
+			path: '/cabinets',
+			component: () => import('layouts/MainLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Cabinets.vue') },
+			],
+		},
+
+		{
+			path: '/times',
+			component: () => import('layouts/MainLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Times.vue') },
+			],
+		},
+
+		{
+			path: '/login',
+			component: () => import('layouts/LoginLayout.vue'),
+			children: [
+				{ path: '', component: () => import('pages/Login.vue') },
+			],
+		},
+
 		// Always leave this as last one,
 		// but you can also remove it
 		{
